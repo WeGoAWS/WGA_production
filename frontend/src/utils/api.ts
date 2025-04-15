73eb6f77dfcec31c4c70f6aee099caca5899ba1a
newbiehwang <137700401+newbiehwang@users.noreply.github.com>
2025-04-15 23:18:53 +0900

    Feat: 프론트엔드 토큰 정보 헤더에 추가

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,11 +1,20 @@
 // src/utils/api.ts
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL, // API Gateway base URL
+  baseURL: import.meta.env.API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore().tokens.accessToken;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
